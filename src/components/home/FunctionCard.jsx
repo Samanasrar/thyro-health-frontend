bf88f4b81ab4b1d20eee91bdb3e13e66b9e9b78b
Saman Asrar <114235394+Samanasrar@users.noreply.github.com>
2025-09-06 16:47:13 +0500

    Update FunctionCard.jsx

--- src/components/home/FunctionCard.jsx
+++ src/components/home/FunctionCard.jsx
@@ -7,13 +7,11 @@ const FunctionCard = ({ title, img, desc, bg }) => {
       style={{ backgroundColor: bg }}
       className="w-full rounded-xl shadow-lg flex flex-col justify-between items-center transition-shadow duration-300 hover:shadow-2xl h-full px-7 py-6 min-h-[320px]"
     >
-      {/* Title */}
+      
       <h2 className="text-md font-semibold text-center">{title}</h2>
 
-      {/* Image */}
       <img src={img} alt={title} className="w-28 h-28 object-contain my-4" />
 
-      {/* Description */}
       <p className="text-sm text-gray-600 text-center flex-grow">
         {desc}
       </p>
